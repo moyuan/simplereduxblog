afa8cefd51dc38e2b9f3655b472281a75d899fb0
Moyuan Chen <moyuan.chen@gmail.com>
2017-11-21 17:31:17 -0500

    add axios

---
+++ src/components/posts_index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        Posts Index
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
