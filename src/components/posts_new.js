26b93f1ccb19d2b52e36c18401287c98d86e5195
Moyuan Chen <moyuan.chen@gmail.com>
2017-11-22 21:52:14 -0500

    redux form continued

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,22 +2,77 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField() {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        {field.meta.error}
+      </div>
+    );
+  }
 
+  onSubmit(values) {
+    // this === component
+    console.log(values);
   }
 
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form action="">
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
+          label="Title"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
         />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Posts Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Save</button>
       </form>
     );
   }
 }
 
+
+function validate(values) {
+  // console.log(values) -> { title: 'adfs', categories: 'fdd', content: 'ididi' }
+  const errors = {};
+
+  // validate the inputs from 'values'
+  // value.title's "title" must match the Field's name
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (values.title != null && values.title.length < 3) {
+    errors.title = "The title is too short";
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter come content please';
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If erros has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
